ae225358b19629bfceb155f4f9b095b1bebc2d8f
Benjamin Leskey <benleskey@mail.mvnu.edu>
2021-11-12 15:39:20 -0500

    Make $ks_db explicit in DB API functions.

--- scripts/admin_setup.php
+++ scripts/admin_setup.php
@@ -47,7 +47,7 @@ printf("Name: %s\nE-mail: %s\n", $name, $email);
 $ok = readline("Does this look OK? (y/n) ");
 if(in_array(strtolower($ok), array('yes', 'y'))) {
 	echo("Adding user...\n");
-	ks_da_user_add(array(
+	ks_da_user_add($ks_db, array(
 		'name' => $name,
 		'email' => $email,
 		'password' => $password,
--- web/header/da_user.php
+++ web/header/da_user.php
@@ -4,8 +4,7 @@ include_once('header.php');
 
 /* Add a new user. Accepts array of params with keys:
 name, email, password, privilege */
-function ks_da_user_add(array $params) {
-	GLOBAL $ks_db;
+function ks_da_user_add($ks_db, array $params) {
 	$ks_db->query('INSERT INTO registered_user (name, email, password, privilege) VALUES ($1, $2, $3, $4)',
 		array($params['name'], $params['email'], password_hash($params['password'], PASSWORD_DEFAULT), $params['privilege']));
 }
