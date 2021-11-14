f91080268b284ff28002939110af866d0a971f63
Isaiah Savage <savageit16@gmail.com>
2021-11-14 17:06:24 -0500

    Reformat Scan Form to Include Error Messages

--- web/scan.php
+++ web/scan.php
@@ -1,20 +1,17 @@
 <?php 
 include_once('header.php');
-$card_id = isset($_GET["card_id"]) ? htmlspecialchars($_GET["card_id"]) : '';
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-		<title><?php echo $ks_config['title'] . ' - New Scan'; ?></title>
-	</head>
-	<body>
-		<h1>Submit a Scan</h1>
+$card_id = isset($_GET["card_id"]) ? htmlspecialchars($_GET["card_id"]) : '';
+
+function show_form()
+{
+    ?>
+        <h1>Submit a Scan</h1>
         <!-- Card ID tied to a card's QR code -->
         <!-- Example: my.website.com/web/scan.php?card_id=1032 -->
         <!-- Perhaps check for manually-injected card_id:
-             if (isset($_GET["source"]) && $_GET["source"] == "qr") {...} 
-             or use some sort of check for a specific format
+            if (isset($_GET["source"]) && $_GET["source"] == "qr") {...} 
+            or use some sort of check for a specific format
         -->
         <h3>Card ID: <?php echo $card_id ?></h3>
         <form action="scan_confirm.php?card_id=<?php echo $card_id ?>" method="post">
@@ -22,7 +19,41 @@ $card_id = isset($_GET["card_id"]) ? htmlspecialchars($_GET["card_id"]) : '';
             <textarea name="input_act" id="input_act" cols="30" rows="10"></textarea><br>
             What did this act mean to you?<br>
             <textarea name="input_meaning" id="input_meaning" cols="30" rows="10"></textarea><br>
-            <button type="submit">Submit</button>
+            <input type="submit" value="Submit">
         </form>
+    <?php
+}
+
+function show_error()
+{
+ ?>
+    <h1>Oops!</h1>
+    <h3>It seems that there was an issue reading your card. Please try scanning the card again.</h3>
+ <?php
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+		<title><?php echo $ks_config['title']; ?> | New Scan</title>
+	</head>
+	<body>
+		
+        <?php 
+            try {
+                $results = $ks_db->query('SELECT card_id FROM card WHERE card_id = $1 AND card_id IN (select card_id from r_u_card)', [$card_id]);
+                if (!empty($results)) {
+                    // Card is already registered; add form to page
+                    show_form();
+                } else {
+                    // TODO: check for card_id IN card AND NOT IN r_u_card
+                    // Add error message to page
+                    show_error();
+                }
+            } catch (Exception $e) {
+                show_error();
+            }
+        ?>
 	</body>
 </html>
