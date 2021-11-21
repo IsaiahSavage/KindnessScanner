8fa8d2592494424c0292412e082553da9321e936
Isaiah Savage <savageit16@gmail.com>
2021-11-21 08:27:13 -0500

    Rework homepage

--- web/index.php
+++ web/index.php
@@ -13,8 +13,29 @@
 		<header>
 			<?php include_once('nav_header.php'); ?>
 		</header>
-		<h1 id="header-dashboard">God Loves Unconditionally</h1>
-		<div id="map" style="width: 100%; height: 75vh;"></div>
-		<?php ks_map_all('map'); ?>
+		<section id="top-dashboard">
+			<h1 class="header-dashboard">God Loves Unconditionally.<br>So should we.</h1>
+			<p class="description-dashboard">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, rem.</p>
+			<a class="button" href="#about-dashboard">Learn More</a>
+			<a class="button" href="#map-dashboard">View Map</a>
+		</section>
+		<section id="about-dashboard">
+			<h1 class="header-dashboard">Stick together, pass it forward.</h1>
+			<p class="description-dashboard">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, dolor.</p>
+			<a class="button" href="#map-dashboard">View Map</a>
+		</section>
+		<section id="map-dashboard">
+			<h1 class="header-dashboard">
+				<?php 
+					GLOBAL $ks_db; 
+					$result = $ks_db->query_array('SELECT count(scan_id) FROM scan');
+					echo $result[0][0] . ' scans and counting.';
+				?>
+			</h1>
+			<p class="description-dashboard">Soon, you'll be able to request a card and help pay it forward.</p>
+			<div id="map"></div>
+			<?php ks_map_all('map'); ?>
+			<a href="#top-dashboard" class="button">Back to Top</a>
+		</section>
 	</body>
 </html>
