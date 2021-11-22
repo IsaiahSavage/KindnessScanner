aba15dd2503b5ae18a59a4419cc9657fc8333e47
Isaiah Savage <61752116+IsaiahSavage@users.noreply.github.com>
2021-11-22 00:57:57 -0500

    CSS Redo (#15)
    * Rework homepage (index.php)
    
    * Redo navigation bar
    
    * Change submit types to match buttons on index.php
    
    * Finish descriptions on index.php

--- web/index.php
+++ web/index.php
@@ -13,8 +13,30 @@
 		<header>
 			<?php include_once('nav_header.php'); ?>
 		</header>
-		<h1 id="header-dashboard">God Loves Unconditionally</h1>
-		<div id="map" style="width: 100%; height: 75vh;"></div>
-		<?php ks_map_all('map'); ?>
+		<section id="top-dashboard">
+			<h1 class="header-dashboard">God Loves Unconditionally.<br>So should we.</h1>
+			<p class="description-dashboard">Acts of kindness can change a life. 
+			<br>Let's work together to spread them as far as possible.</p>
+			<a class="button" href="#about-dashboard">Learn More</a>
+			<a class="button" href="#map-dashboard">View Map</a>
+		</section>
+		<section id="about-dashboard">
+			<h1 class="header-dashboard">Stick together, pay it forward.</h1>
+			<p class="description-dashboard">Kindness Scanner is built around the idea that a simple act of kindness can mean the world to the one who receieves it. By passing a GLU card, you can add an act of kindness to our map - or perhaps start a chain of many.</p>
+			<a class="button" href="#map-dashboard">View Map</a>
+		</section>
+		<section id="map-dashboard">
+			<h1 class="header-dashboard">
+				<?php 
+					GLOBAL $ks_db; 
+					$result = $ks_db->query_array('SELECT count(scan_id) FROM scan');
+					echo $result[0][0] . ' scans and counting.';
+				?>
+			</h1>
+			<p class="description-dashboard">Soon, you'll be able to request a card and help pay it forward.</p>
+			<div id="map"></div>
+			<?php ks_map_all('map'); ?>
+			<a href="#top-dashboard" class="button">Back to Top</a>
+		</section>
 	</body>
 </html>
--- web/nav_header.php
+++ web/nav_header.php
@@ -4,24 +4,34 @@
 ?>
 <link rel="stylesheet" href="styles.css">
 <nav class="navigation">
+	<div id="wrapper-links-nav">
+		<a id="link-dashboard-nav" href='index.php'><?php echo $ks_config['title']; ?></a>
+		<a id="link-about-nav" href="index.php#about-dashboard">About</a>
+		<a id="link-map-nav" href="index.php#map-dashboard">Map</a>
+	</div>
 	<?php
 		if($ks_logged_in_user) {
-			printf("<li>Hello, %s</li>\n", htmlentities(explode(" ", $ks_logged_in_user['name'])[0]));
 			?>
-				<a href='index.php' id="link-dashboard">Home</a>
-				<a href='cards_manager.php'>Manage Cards</a>
-				<h1 id="title"><?php echo($ks_config['title']); ?></h1>
-				<a href='user_manager.php?user_id=<?php echo($ks_logged_in_user['id']); ?>'>My Settings</a>
-				<?php
-					if(ks_can_manage_users()) {
-						?> <a href='users_manager.php'>Manage Users</a> <?php
-					}
-				?>
-				<a href='logout.php'>Logout</a>
+				<!-- Drop Down Menu -->
+				<div class="dropdown" id="wrapper-dropdown-nav">
+					<a class="dropdown-title" id="welcome-dropdown-nav" href="#"><?php printf("Hello, %s", htmlentities(explode(" ", $ks_logged_in_user['name'])[0])); ?></a>
+					<div class="dropdown-sub" id="content-dropdown-nav">
+						<a class="dropdown-content" id="cards-dropdown-nav" href='cards_manager.php'>My Cards</a>
+						<a class="dropdown-content" id="settings-dropdown-nav" href='user_manager.php?user_id=<?php echo($ks_logged_in_user['id']); 
+							?>'>Settings</a>
+						<?php
+							if(ks_can_manage_users()) {
+								?> <a class="dropdown-content" id="users-dropdown-nav" href='users_manager.php'>Manage Users</a> <?php
+							}
+						?>
+						<a class="dropdown-content" id="logout-dropdown-nav" href='logout.php'>Logout</a>
+					</div>
+				</div>
+				
 			<?php
 		}
 		else {
-			?> <a href='login.php'>Login</a> <?php
+			?> <a id="link-login-nav" href='login.php'>Login</a> <?php
 		}
 	?>
 </nav>
